2e1eea7881ea35bfa6dfa78a0bc1c0140d627b54
Aftal-web <agobori12@gmail.com>
2020-05-31 10:21:25 -0700

    Updates

---
+++ precache-manifest.964b0dbad06c162fb488df9cb796c055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0c16ebaf8a9bbf2ad74bd7fb877d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c005da6ef65f46b6c97",
+    "url": "/static/css/main.a609d0ac.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c005da6ef65f46b6c97",
+    "url": "/static/js/main.acd0dc28.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
